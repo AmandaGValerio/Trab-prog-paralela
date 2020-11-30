ed032e42f9488fc4555851cd13550d82ab4b275e
Amanda Valério <56452064+AmandaGValerio@users.noreply.github.com>
2020-11-30 18:26:38 -0300

    iniciando projeto
    adicionando os arquivos que são o esqueleto do projeto

---
+++ Atv1-prog-paralela/README.md
@@ -0,0 +1,1 @@
+#Trab-prog-paralela
---
+++ Atv1-prog-paralela/src/codigos/Main.java
@@ -0,0 +1,16 @@
+package codigos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//cliente envia para o servidor uma imagem
+		//separar a imagem em 4 pedaços
+		//cada thread fica com um pedaço
+		//os pixels são convertidos tirando a média
+		//dps que termina as threads juntam td e formam a imagem
+		//a imagem é salva em uma pasta e exibida na tela
+	}
+
+}
