0676e317d7b82850cf9247c6813d2291464b5d3e
AmandaGValerio <valeriogamanda@gmail.com>
2020-12-01 11:03:13 -0300

    adicionando conexões
    Estes códigos foram retirados do exemplo de sockets da aula de 20/11

--- Atv1-prog-paralela/src/codigos/Cliente.java
+++ Atv1-prog-paralela/src/codigos/Cliente.java
@@ -1,5 +1,31 @@
+//autores: Amanda, Lucas Alves
+
 package codigos;
 
-public class Cliente {
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class Cliente {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			//conectando no servidor
+			Socket cliente = new Socket("127.0.0.1", 8090);
+			//se conectado, exibe uma mensagem
+			System.out.println("Cliente conectado!");
+			
+			//enviar a imagem aqui
+			
+			//fechando as conexoes
+			cliente.close();
+		} 
+		catch (UnknownHostException e) {
+			e.printStackTrace();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- Atv1-prog-paralela/src/codigos/Servidor.java
+++ Atv1-prog-paralela/src/codigos/Servidor.java
@@ -1,10 +1,43 @@
+//autores: Amanda, Lucas Alves
+
 package codigos;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Servidor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			//iniciando o servidor
+			ServerSocket servidor = new ServerSocket(8090);
+			System.out.println("Servidor em execução na porta 8090...");
+			System.out.println("Servidor aguardando requisições...");
+			
+			//esperando por uma conexao
+			Socket cliente = servidor.accept();
+			
+			//se uma conexão for estabelecida
+			System.out.println("Nova conexão estabelecida com o cliente " +
+					cliente.getInetAddress().getHostAddress());
+			
+			// ler a imagem 
+			
+			//dividir em 4 threads
+			
+			//executar o algoritmo nas 4 threads ao mesmo tempo
+			
+			//retornar a imagem em preto e branco
+			
+			//fechando as conexoes
+			servidor.close();
+			cliente.close();
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
 	}
 
 }
