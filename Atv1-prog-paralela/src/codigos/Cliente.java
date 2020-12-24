c289b65d8b0d67c41046f63031562b02ddbab9ce
AmandaGValerio <valeriogamanda@gmail.com>
2020-12-24 01:12:53 -0300

    codigo para reenviar o arquivo
    codigo para reenviar a imagem de volta e salvar na pasta

--- Atv1-prog-paralela/src/codigos/Cliente.java
+++ Atv1-prog-paralela/src/codigos/Cliente.java
@@ -7,14 +7,14 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFileChooser;
 
 public class Cliente {
 	
@@ -26,21 +26,53 @@ public class Cliente {
 			//se conectado, exibe uma mensagem
 			System.out.println("Cliente conectado!");
 			
-			//enviar a imagem aqui
-//			JFileChooser arq = new JFileChooser();
-//			File f;
-//			ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-//			arq.showOpenDialog(null);
-//	        f = arq.getSelectedFile();
-//	        String foto = f.getAbsolutePath();
+			//enviar a imagem aqui	        
+			FileInputStream fis = null;
+		    BufferedInputStream bis = null;
+		    OutputStream os = null;
+			File myFile = new File ("imagem.jpg");
+			byte [] mybytearray  = new byte [(int)myFile.length()];
+			fis = new FileInputStream(myFile);
+			bis = new BufferedInputStream(fis);
+			bis.read(mybytearray,0,mybytearray.length);
+			os = cliente.getOutputStream();
+			os.write(mybytearray,0,mybytearray.length);
+			os.flush();
+			System.out.println("Done.");
 			
-			BufferedInputStream arquivo = new BufferedInputStream(new FileInputStream("imagem.jpg"));
-	        
-//	        BufferedImage imagem = ImageIO.read(new File("imagem.jpg"));
-	        
+			
+			//recebendo a imagem
+			int bytesRead;
+			int FILE_SIZE = 72000; //o tamanho deve ser suficiente para comportar o arquivo, já que o DO não funciona
+		    int current;
+		    FileOutputStream fos = null;
+		    BufferedOutputStream bos = null;
+		    System.out.println("entrei no try");
+			byte [] mybytearray2  = new byte [FILE_SIZE];
+			InputStream is = cliente.getInputStream();
+			fos = new FileOutputStream("imagensResultantes/recebida.jpg");
+			bos = new BufferedOutputStream(fos);
+			bytesRead = is.read(mybytearray2, 0, mybytearray2.length);
+			current = bytesRead;
+			
+			//por algum motivo que eu não consegui descobrir, o DO começou a dar um loop infinito 
+			//e não está atualizando as variaveis internas 
+//			do {
+//				bytesRead = is.read(mybytearray2, current, (mybytearray2.length - current));
+//				if(bytesRead >= 0) { 
+//					current += bytesRead;
+//				}
+//			} while(bytesRead > -1);
+//			bos.write(mybytearray2, 0 , current);			
+			bos.write(mybytearray2); 
+			bos.flush();
+			System.out.println("recebi");
 			
 			
 			//fechando as conexoes
+			if (bis != null) bis.close();
+			if (os != null) os.close();
+			bos.close();
 			cliente.close();
 		} 
 		catch (UnknownHostException e) {
--- Atv1-prog-paralela/src/codigos/Servidor.java
+++ Atv1-prog-paralela/src/codigos/Servidor.java
@@ -4,11 +4,16 @@ package codigos;
 
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -31,32 +36,42 @@ public class Servidor{
 			System.out.println("Nova conexão estabelecida com o cliente " +
 					cliente.getInetAddress().getHostAddress());
 			
-			// ler a imagem 
-			byte[] objectAsByte = new byte[cliente.getReceiveBufferSize()];
-	        BufferedInputStream bf = new BufferedInputStream(cliente.getInputStream());
-	        bf.read(objectAsByte);
-			ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
-	        		
-			Object obj = null;
-		     ByteArrayInputStream bis = null;
-		     ObjectInputStream ois = null;
-		     bis = new ByteArrayInputStream(objectAsByte);
-		     ois = new ObjectInputStream(bis);
-		     try {
-				obj = ois.readObject();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-	        bis.close();
-	        ois.close();
-	        BufferedImage imagem = (BufferedImage) obj;
+			// ler a imagem 			
+			int bytesRead;
+			int FILE_SIZE = 65536; //o tamanho deve ser suficiente para comportar o arquivo, já que o DO não funciona
+		    int current;
+		    FileOutputStream fos = null;
+		    BufferedOutputStream bos = null;
+		    System.out.println("entrei no try");
+			byte [] mybytearray  = new byte [FILE_SIZE];
+			InputStream is = cliente.getInputStream();
+			fos = new FileOutputStream("ima.jpg");
+			bos = new BufferedOutputStream(fos);
+			bytesRead = is.read(mybytearray, 0, mybytearray.length);
+			current = bytesRead;
+			System.out.println("input " + bytesRead + " atual " + current);
+			//por algum motivo que eu não consegui descobrir, o DO começou a dar um loop infinito 
+			//e não está atualizando as variaveis internas 
+//			do {
+//				System.out.println("input " + bytesRead + " atual " + current);
+//				bytesRead = is.read(mybytearray, current, (mybytearray.length - current));
+//				if(bytesRead >= 0) { 
+//					current += bytesRead;
+//				}
+//			} while(bytesRead > -1);
+//			System.out.println("sai do try");
+//			bos.write(mybytearray, 0 , current);			
+			bos.write(mybytearray); 
+			bos.flush();
+			System.out.println("recebi");
+			
 			
 //			esta linha serve apenas para testar o algoritmo		
-			BufferedImage imagen = ImageIO.read(new File("imagem.jpg"));
+			BufferedImage imagem = ImageIO.read(new File("ima.jpg"));
 		
 			//dividindo a imagem
-			int largura = imagen.getWidth();
-	        int altura = imagen.getHeight();
+			int largura = imagem.getWidth();
+	        int altura = imagem.getHeight();
 	        int larMeio, altMeio;
 	        larMeio = largura/2;
 	        altMeio = altura/2;
@@ -66,10 +81,10 @@ public class Servidor{
 	                
 	        
 	        //dividir em 4 threads
-	        ConverterImg thread1 = new ConverterImg(0, altMeio, 0, larMeio, imagen, processada); 
-	        ConverterImg thread2 = new ConverterImg(0, altMeio, larMeio, largura, imagen, processada); 
-	        ConverterImg thread3 = new ConverterImg(altMeio, altura, 0, larMeio, imagen, processada); 
-	        ConverterImg thread4 = new ConverterImg(altMeio, altura, larMeio, largura, imagen, processada);  
+	        ConverterImg thread1 = new ConverterImg(0, altMeio, 0, larMeio, imagem, processada); 
+	        ConverterImg thread2 = new ConverterImg(0, altMeio, larMeio, largura, imagem, processada); 
+	        ConverterImg thread3 = new ConverterImg(altMeio, altura, 0, larMeio, imagem, processada); 
+	        ConverterImg thread4 = new ConverterImg(altMeio, altura, larMeio, largura, imagem, processada);  
 	        
 			//executar o algoritmo nas 4 threads ao mesmo tempo
 	        thread1.run();
@@ -77,9 +92,30 @@ public class Servidor{
 	        thread3.run();
 	        thread4.run();
 
-
-			//retornar a imagem em preto e branco
+			//salvar a imagem em preto e branco
 	        ImageIO.write(processada, "jpg", new File("pretobranco.jpg"));
+	        
+	        //retornar a imagem
+	        FileInputStream fis = null;
+		    BufferedInputStream bis = null;
+		    OutputStream os = null;
+			File myFile = new File ("pretobranco.jpg");
+			byte [] mybytearray1  = new byte [(int)myFile.length()];
+			fis = new FileInputStream(myFile);
+			bis = new BufferedInputStream(fis);
+			bis.read(mybytearray1,0,mybytearray1.length);
+			os = cliente.getOutputStream();
+			os.write(mybytearray1,0,mybytearray1.length);
+			os.flush();
+			System.out.println("Done2.");
+			
+			
+			
+			if (fos != null) fos.close();
+			if (bos != null) bos.close();
+			if (bis != null) bis.close();
+			if (os != null) os.close();
+	        
 			
 			//fechando as conexoes
 			servidor.close();
